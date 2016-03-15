292233355543accbe110e954436c17d7878c3924
Josiah <jnjuki103@gmail.com>
2016-03-15 19:09:53 +0300

    added theme files. To be refactored later

--- configuration/settings.py
+++ configuration/settings.py
@@ -125,4 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'static'),
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
