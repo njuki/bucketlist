c45554a136db27df3b3f12c9abf0d8a814d1fee2
Josiah <jnjuki103@gmail.com>
2016-03-17 16:39:05 +0300

    Added more tests to the authentication suite

--- blist_api/tests/test_api.py
+++ blist_api/tests/test_api.py
@@ -20,15 +20,37 @@ class BucketListTestCase(APITestCase):
     
     def setUp(self):
         self.url = reverse("bucket-list")
-        self.user = factories.UserFactory()
+        self.user_factory = factories.UserFactory()
         
     
     def test_authorized_access(self):
         """Tests that only authenticated users can access the endpoint"""
 
-        # should fail for non-logged in user
+        # Should fail for non-logged in user
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 401)
 
         response = self.client.post(self.url)
         self.assertEqual(response.status_code, 401)
+        
+        # Get auth_token
+        auth_url = reverse("token-auth")
+        credentials = {
+                       "username": self.user_factory.username,
+                       "password": "default"
+        }
+        
+        response = self.client.post(auth_url, data=credentials)
+        auth_token = response.data['token']
+        
+        # Use the auth_token obtained to access authenticate on the endpoint
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + auth_token)
+        
+        # Should go through since the token is valid
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        
+        # Authenitcation should pass but then fail with a bad request response
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 400)
+        
--- blist_api/urls.py
+++ blist_api/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from blist_api import views as api_views
 
 urlpatterns = [
-    url(r'^auth/login$', api_views.AuthenticationTokenView.as_view(), name='get-token'),
+    url(r'^auth/login$', api_views.AuthenticationTokenView.as_view(), name='token-auth'),
     url(r'^bucketlists/$', api_views.BucketList.as_view(), name='bucket-list'),
 ]
